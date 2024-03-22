6f075e7091e0d1a88e2fb819f633fd420d80c66f
MoonSsunny <moonsun116@naver.com>
2024-03-22 22:56:23 +0900

    Chapter03 과제B_문선주

--- packages/chapter2/src/hooks.js
+++ packages/chapter2/src/hooks.js
@@ -1,15 +1,47 @@
 export function createHooks(callback) {
+  let states = [];
+  let memoizedValues = [];
+  let callsCount = 0;
+
   const useState = (initState) => {
-    return [];
+    const callIndex = callsCount;
+    callsCount++;
+
+    if (states[callIndex] === undefined) {
+      states[callIndex] = initState;
+    }
+
+    const setState = (newValue) => {
+      if (newValue === states[callIndex]) return;
+
+      states[callIndex] = newValue;
+      callback();
+    };
+
+    return [states[callIndex], setState];
   };
 
-  const useMemo = (fn, refs) => {
-    return fn();
+  const useMemo = (fn, deps) => {
+    const callIndex = callsCount;
+    callsCount++;
+
+    if (memoizedValues[callIndex] === undefined) {
+      memoizedValues[callIndex] = { deps, value: fn() };
+    } else {
+      const hasChangedDeps = deps.some(
+        (dep, i) => memoizedValues[callIndex].deps[i] !== dep
+      );
+      if (hasChangedDeps) {
+        memoizedValues[callIndex] = { deps, value: fn() };
+      }
+    }
+
+    return memoizedValues[callIndex].value;
   };
 
   const resetContext = () => {
-
-  }
+    callsCount = 0;
+  };
 
   return { useState, useMemo, resetContext };
 }
